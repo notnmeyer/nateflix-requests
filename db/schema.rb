b4198cb617bd36c4539f1a944af7362761eae83a
Nate Meyer <672246+notnmeyer@users.noreply.github.com>
2023-03-29 08:00:25 -0700

    style more things, set a default issue status

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_29_054251) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_29_144350) do
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -26,7 +26,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_29_054251) do
   create_table "issues", force: :cascade do |t|
     t.string "title"
     t.text "notes"
-    t.integer "status"
+    t.integer "status", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
