c31dee7abadb6b2fbec92d56c3b4aa3bba7f9305
Nate Meyer <672246+notnmeyer@users.noreply.github.com>
2023-03-29 10:17:38 -0700

    add user and role to header

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,13 @@
 class ApplicationController < ActionController::Base
+  def current_user
+    return current_admin if admin_signed_in?
+
+    super
+  end
+
   def authenticate_user!
     return if admin_signed_in?
+
     super
   end
 end
