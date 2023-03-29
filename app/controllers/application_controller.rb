542d262cb07e48be88c756ac0f85356aaf9ea3cc
Nate Meyer <672246+notnmeyer@users.noreply.github.com>
2023-03-28 22:14:54 -0700

    add devise, style things

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,6 @@
 class ApplicationController < ActionController::Base
+  def authenticate_user!
+    return if admin_signed_in?
+    super
+  end
 end
--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -1,4 +1,6 @@
 class RequestsController < ApplicationController
+  before_action :authenticate_admin!, only: [:destroy, :update, :edit]
+  
   def index
     @requests = Request.all
   end
@@ -12,12 +14,31 @@ class RequestsController < ApplicationController
     if @request.save
       redirect_to root_path, notice: "Request was successfully created."
     else
-      render :new
+      redirect_to root_path, alert: "There was a problem submitting your request."
     end
+  end
+
+  def destroy
+    @request = Request.find(params[:id])
+    @request.destroy
+    redirect_to root_path, notice: "Request was successfully deleted."
+  end
+
+  def update
+    @request = Request.find(params[:id])
+    if @request.update(request_params)
+      redirect_to root_path, notice: "Request was successfully updated."
+    else
+      redirect_to root_path, alert: "There was a problem updating your request."
+    end
+  end
+
+  def edit
+    @request = Request.find(params[:id])
   end
 
   private
     def request_params
-      params.require(:request).permit(:title, :media_type, :notes)
+      params.require(:request).permit(:title, :media_type, :notes, :status)
     end
 end
