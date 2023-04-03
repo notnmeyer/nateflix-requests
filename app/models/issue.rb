25dc3325c59f88b6fbf71cc959ca2cea568cd40a
Nate Meyer <672246+notnmeyer@users.noreply.github.com>
2023-04-02 21:57:02 -0700

    issue model tests

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -1,6 +1,5 @@
 class Issue < ApplicationRecord
   validates :title, presence: true
-  validates :notes, presence: true
 
   enum status: { reported: 0, resolved: 1, wontfix: 2 }
 end
--- test/models/issue_test.rb
+++ test/models/issue_test.rb
@@ -1,7 +1,21 @@
-require "test_helper"
+require 'test_helper'
 
 class IssueTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'invalid without title' do
+    issue = Issue.new(notes: '', status: 'reported')
+    assert_not issue.valid?
+  end
+
+  test 'notes can be blank' do
+    issue = Issue.new(title: 'Test', notes: '', status: 'reported')
+    assert issue.valid?
+  end
+
+  test 'status can be omitted' do
+    issue = Issue.new(title: 'Test', notes: '')
+    assert issue.valid?
+
+    issue.save
+    assert_equal 'reported', issue.status
+  end
 end
