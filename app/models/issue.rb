1c7413f9184a89d30ad2025eaaaee975192d775a
Nate Meyer <672246+notnmeyer@users.noreply.github.com>
2023-04-03 15:11:42 -0700

    require notes, but allow it to be blank

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -1,5 +1,6 @@
 class Issue < ApplicationRecord
   validates :title, presence: true
+  validates :notes, length: { maximum: 500 }, allow_blank: true
 
   enum status: { reported: 0, resolved: 1, wontfix: 2 }
 end
