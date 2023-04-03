9b690a5e3402cfeee1ddf0537c17a593f4e1f2c1
Nate Meyer <672246+notnmeyer@users.noreply.github.com>
2023-04-03 15:11:10 -0700

    requests model tests

--- test/models/request_test.rb
+++ test/models/request_test.rb
@@ -1,7 +1,26 @@
-require "test_helper"
+require 'test_helper'
 
 class RequestTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'invalid without title' do
+    request = Request.new(notes: '', status: 'pending')
+    assert_not request.valid?
+  end
+
+  test 'notes can be blank' do
+    request = Request.new(title: 'Test', notes: '', status: 'pending', media_type: 'tv')
+    assert request.valid?
+  end
+
+  test 'invalid without media_type' do
+    request = Request.new(title: 'Test', notes: '')
+    assert_not request.valid?
+  end
+
+  test 'status can be omitted' do
+    request = Request.new(title: 'Test', notes: '', media_type: 'tv')
+    assert request.valid?
+
+    request.save
+    assert_equal 'pending', request.status
+  end
 end
