9d9d8c8553d16d816a2fbfab20737ef647e8f6eb
Nate Meyer <672246+notnmeyer@users.noreply.github.com>
2023-04-15 16:35:22 -0700

    now that theres a history page to view everything, shorten the cutoff on the dashboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,7 +2,7 @@ class DashboardController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @requests_cutoff_days = 7
+    @requests_cutoff_days = 3
     @requests = Request.where('status = ? OR created_at > ?', 'pending', @requests_cutoff_days.days.ago)
     @request = Request.new
 
