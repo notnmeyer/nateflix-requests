e976e3bdbfcffc19a164f5863d7ebe101747bf74
Nate Meyer <672246+notnmeyer@users.noreply.github.com>
2023-04-15 11:53:11 -0700

    switch to "sign in/out", update routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,12 @@ Rails.application.routes.draw do
   resources :issues
   resources :requests
 
-  devise_for :users
+  devise_for :users, skip: :all
+  devise_scope :user do
+    get 'sign_in', to: 'devise/sessions#new', as: :new_user_session
+    post 'sign_in', to: 'devise/sessions#create', as: :user_session
+    delete 'sign_out', to: 'devise/sessions#destroy', as: :destroy_user_session
+  end
 
   root 'dashboard#index'
   get '/', to: 'dashboard#index', as: 'dashboard'
