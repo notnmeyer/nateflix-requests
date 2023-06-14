ba26818a883fea851b877b8ade32a1692d4ed9e3
Nate Meyer <672246+notnmeyer@users.noreply.github.com>
2023-06-13 19:41:18 -0700

    add wontfill status to requests

--- app/models/request.rb
+++ app/models/request.rb
@@ -7,5 +7,5 @@ class Request < ApplicationRecord
   validates :notes, length: { maximum: 500 }, allow_blank: true
 
   enum media_type: { tv: 0, movie: 1 }
-  enum status: { pending: 0, filled: 1 }
+  enum status: { pending: 0, filled: 1, wontfill: 2 }
 end
